3bdec86bae194a270527d1a731ea3ec88176318f
John Ron <kikobuere27@gmail.com>
2024-09-17 15:00:55 +0800

    Change the axios defaults base url to the main url of the front end

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,4 +2,4 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://students.42web.io";
